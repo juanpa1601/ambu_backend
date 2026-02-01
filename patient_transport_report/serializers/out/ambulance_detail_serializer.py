0ef3cf86da62a14bcda445f0dfcde1c59c436982
Juan Pablo Rodriguez Velasquez <jrodriguez@deputizeamerica.com>
2026-02-01 01:15:41 -0500

    Display name en serializer de ambulance

--- patient_transport_report/serializers/out/ambulance_detail_serializer.py
+++ patient_transport_report/serializers/out/ambulance_detail_serializer.py
@@ -3,13 +3,22 @@ from daily_monthly_inventory.models import Ambulance
 
 class AmbulanceDetailSerializer(serializers.ModelSerializer):
     '''Serializer for ambulance details.'''
-    
+
+    display_name: serializers.SerializerMethodField = serializers.SerializerMethodField()
+
     class Meta:
         model = Ambulance
         fields = [
             'id',
             'mobile_number',
             'license_plate',
-            'is_active'
+            'is_active',
+            'display_name'
         ]
         read_only_fields = fields
+
+    def get_display_name(
+        self, 
+        obj: Ambulance
+    ) -> str:
+        return f'Móvil {obj.mobile_number} - {obj.license_plate}'
